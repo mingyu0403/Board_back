afabbf99807647ed70f4976974e3e705c96d395c
mingyu0403 <jmg6017@naver.com>
2019-08-08 16:03:22 +0900

    게시판 백엔드 첫 커밋

---
+++ src/main/java/kr/hs/dgsw/board_back/Controller/BoardController.java
@@ -0,0 +1,10 @@
+package kr.hs.dgsw.board_back.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+
+}
